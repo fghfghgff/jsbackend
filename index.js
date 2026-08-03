acb3bf3ddc40e0b72ce065f67d4a03f4ec0f8f49
fghfghgff <tiagoromanor@outlook.com>
2026-08-03 12:01:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const moduloMatematica = require('./calculadora');
+// Conceito visual de Requisição e Resposta no Back-End:
+// Cliente faz:  GET /produtos
+// Servidor faz: Processa no banco de dados e retorna JSON
+const respostaServidor = {
+  status: 200,
+  mensagem: "Pedido recebido na cozinha do servidor!",
+  dados: ["Prato 1", "Prato 2"]
+};
 
-console.log("Resultado da soma via Módulo:", moduloMatematica.somar(10, 25));
-console.log("Resultado da multiplicação:", moduloMatematica.multiplicar(4, 5));
+console.log(respostaServidor);
